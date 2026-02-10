122e382aff1732b0ef459c4fecd28f1152d0c885
Developer-Sohail786 <sanu30963@gmail.com>
2026-02-10 21:07:04 +0530

    Add div wrapper to Home component
    Wrap Manager component in a div for layout.

--- Password Manager(local)/src/Pages/Home.jsx
+++ Password Manager(local)/src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import Manager from "../Components/Manager";
 
-
 const Home = () => {
   return (
-    <>
+    <div className="min-h-screen">
       <Manager />
-      
-  
-    </>
+    </div>
   );
 };
 
 export default Home;
+
